d6d98e13e981ae943bb53ba00d1983a9847069c4
Mattslots <dadex17092@gmail.com>
2026-07-29 09:30:56 +0200

    Delete src directory

--- src/utils/find.js
+++
@@ -1,4 +0,0 @@
-function normalize(v=""){return v.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g,"");}
-function findTextChannel(guild,names){const wanted=names.map(normalize);return guild.channels.cache.find(c=>c.isTextBased()&&wanted.includes(normalize(c.name)));}
-function findRoleByName(guild,name){return guild.roles.cache.find(r=>r.name===name);}
-module.exports={normalize,findTextChannel,findRoleByName};
