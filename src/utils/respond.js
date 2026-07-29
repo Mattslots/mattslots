938184e66ba1c70d2ebf4c37dc4b442702d12600
Mattslots <dadex17092@gmail.com>
2026-07-29 09:41:29 +0200

    Delete src directory

--- src/utils/respond.js
+++
@@ -1,8 +0,0 @@
-async function safeReply(interaction, payload) {
-  const data = typeof payload === "string" ? { content: payload, ephemeral: true } : payload;
-  if (interaction.deferred) return interaction.editReply(data);
-  if (interaction.replied) return interaction.followUp(data);
-  return interaction.reply(data);
-}
-
-module.exports = { safeReply };
